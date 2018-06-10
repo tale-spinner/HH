385d7efb545219901edc0cc185a6fba6aae9bde0
Jack <>
2018-06-09 22:08:00 -0700

    Remove the old ClearHarem code

--- HH/src/com/hh/collector/HHCollector.java
+++ HH/src/com/hh/collector/HHCollector.java
@@ -7,19 +7,6 @@ import java.awt.Robot;
 import java.awt.event.MouseEvent;
 
 
-//Girl 4, 0 scrolls, y=659, s=n/a
-//Girl 5, 1 scroll , y=700, s=87
-//Girl 6, 2 scrolls, y=732, s=96
-//Girl 6, 3 scrolls, y=632, s=100
-//Girl 7, 4 scrolls, y=653, s=107
-//Girl 8, 5 scrolls, y=669, s=112
-//Girl 9, 6 scrolls, y=680, s=117
-//Girl 10, 7 scrolls, y=687, s=121
-//Girl 11, 8 scrolls, y=690, s=125
-//girl 12, 9 scrolls, y=690, s=128
-//girl 13, 10 scrolls, y=687, 
-
-
 public class HHCollector {
     // Constants
     private static final boolean debug   = false;   // Debug mode
@@ -51,7 +38,7 @@ public class HHCollector {
             for(int i=1; i<=12; i++){
                 msg("Cycle " + o + '-' + i, true );
                 
-                ClearHarem2();
+                ClearHarem();
                 
                 try { Thread.sleep(cycleBuffer); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
             }
@@ -71,7 +58,7 @@ public class HHCollector {
         try { Thread.sleep(4000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
     }
     
-    public static void ClearHarem2() {
+    public static void ClearHarem() {
         // Set position of the top of the harem. This is our reference.
         final int topPosX    = 2500;
         final int topPosY    = 245;  // lowest Y of harem is 765
@@ -93,79 +80,6 @@ public class HHCollector {
             }
             scroll(3);
             try { Thread.sleep(300); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
-        }
-    }
-    
-    public static void ClearHarem() {
-        // Set position of top button. This is our reference
-        final int topBtnX    = 2500;
-        final int topBtnY    = 275;
-        
-        // Distance a single click on the scroll wheel takes us
-        int scrollDist = 122;  // Originally 115
-        
-        // Our current Y position
-        int curY = topBtnY;
-        
-        
-        // Reset our location
-        GoToTown();
-
-        // Go to Harem from Town
-        klick( 3575, 650 );
-        try { Thread.sleep(2000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
-        klick( 3575, 650 );
-        try { Thread.sleep(2000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
-        
-      // Scroll through the girls and collect money
-      for( int i=1; i <= 20; i++ ){
-          // First button doesn't need to change positions
-          if( i > 1 ){ curY += 128; }
-          
-          if( i==5  ) { scroll(1); curY-=87; }
-          if( i==6  ) { scroll(1); curY-=96; scroll(1); curY-=100; }
-          if( i==7  ) { scroll(1); curY-=107; }
-          if( i==8  ) { scroll(1); curY-=112; }
-          if( i==9  ) { scroll(1); curY-=117; }
-          if( i==10 ) { scroll(1); curY-=122; }
-//          if( i==11 ) { scroll(1); curY-=125; }
-//          if( i==12 ) { scroll(1); curY-=128; }
-//          if( i==13 ) { scroll(1); curY-=131; }
-//          if( i==14 ) { scroll(1); curY-=134; }
-//          if( i==15 ) { scroll(1); curY-=137; }
-//          if( i==16 ) { scroll(1); curY-=140; }
-//          if( i==17 ) { scroll(1); curY-=143; }
-//          if( i==18 ) { scroll(1); curY-=146; }
-          
-          if( curY > 710 ) { scroll(1); curY-=scrollDist; scrollDist+=3; }
-          msg("i=" + i + "  scrollDist=" + scrollDist );
-        
-        
-//        // Scroll through the girls and collect money
-//        for( int i=1; i <= numGirls; i++ ){
-//            // First button doesn't need to change positions
-//            if( i > 1 ){ curY += 128; }
-//            
-//            // Y=755 is the bottom edge. If we're beyond that, scroll until we're above that and then subtract 115 pixels for every time we scrolled
-//            int safety = 0;
-//            while( curY > 710 ) {
-//                scroll( 1 );
-//                curY -= scrollDist;  // Compensate for scrolling
-//                
-//                if(safety++>3){msg("Shit went sideways!");System.exit(1);}
-//            }
-//            
-//            // There seems to be some slippage over time. Attempt to compensate.
-//            if( i == 25 ) { curY -= 5; }
-//            
-//            // Collect money
-//            if( i == numGirls ) {
-//                klick( topBtnX, 700 );         // Last button is at Y position 700
-//            }
-//            else {
-                klick( topBtnX, curY );
-//            }
-            try { Thread.sleep(800); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
         }
     }
 
